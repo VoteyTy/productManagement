cac7b5837905cca07ca66bb21c57614ab61eddbe
Ty Votey <votey@riseupcambodia-no-MacBook-Pro.local>
2014-06-30 08:25:43 +0700

    add home page

---
+++ product_management/product_management/HomeView.m
@@ -0,0 +1,38 @@
+//
+//  HomeView.m
+//  product_management
+//
+//  Created by Ty Votey on 6/25/14.
+//  Copyright (c) 2014 Ty Votey. All rights reserved.
+//
+
+#import "HomeView.h"
+
+@interface HomeView ()
+
+@end
+
+@implementation HomeView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
